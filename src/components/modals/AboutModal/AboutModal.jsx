65f3bfbc1831ae56ccd5e22fd9d6a4c00b4b39cd
ivars <ivars@ivars.lv>
2023-03-18 16:48:54 +0200

    pievienots kaut kas līdzīgs gŗūtibas pakāpei

--- src/components/modals/AboutModal/AboutModal.jsx
+++ src/components/modals/AboutModal/AboutModal.jsx
@@ -1,11 +1,47 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import {Modal, Button, Divider} from 'antd'
+import {Modal, Button, Divider, Switch} from 'antd'
+
+import {data} from '../../../assets/vocabulary'
 
 export class AboutModal extends Component {
+
+    state = {
+          wordsLevel: {
+            'easy': 0,
+            'middle': 0,
+            'hard': 0
+        }
+    }
+    constructor(props) {
+        super(props)
+
+        const {wordsLevel} = this.state
+
+        data.vocabulary.forEach(item => {
+            if(item.level===1) wordsLevel.easy = wordsLevel.easy + 1
+            if(item.level===2) wordsLevel.middle= wordsLevel.middle + 1
+            if(item.level===3) wordsLevel.hard = wordsLevel.hard + 1
+        })
+
+        this.state = {
+            wordsLevel: {...wordsLevel}
+        }
+    }
+
+    changeMiddle = (checked) => {
+        const { changeLevel } = this.props
+        changeLevel(checked, 'middle')
+    }
+    changeHard= (checked) => {
+        const { changeLevel } = this.props
+        changeLevel(checked, 'hard')
+    }
+
     render() {
-        const {title, open, closeModal} = this.props
+        const {wordsLevel} = this.state
+        const {title, open, closeModal, level } = this.props
         return (
             <Modal
                 title={title}
@@ -40,6 +76,12 @@ export class AboutModal extends Component {
                         Ir iespējams noskaidrot minamā vārda skaidrojumu, bet
                         tad par šo vārdu punktus vairs nav iespējams iegūt
                     </li>
+                </ul>
+                <Divider orientation="left">Spēles vārdnīca </Divider>
+                <ul style={{listStyle: "none"}}>
+                    <li><Switch disabled defaultChecked size={"small"} /> Spēlē iekļauti salīdzinoši vienkārši vārdi ({wordsLevel.easy})</li>
+                    <li><Switch checked={level.middle} onChange={this.changeMiddle} size={"small"} /> Spēlē iekļauti zināmi vārdi ({wordsLevel.middle}), bet retāk lietoti</li>
+                    <li><Switch checked={level.hard}  onChange={this.changeHard}  size={"small"} /> Spēlē būs arī mazāk zināmi vārdi ({wordsLevel.hard})</li>
                 </ul>
             </Modal>
         )
